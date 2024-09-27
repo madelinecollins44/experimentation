4e9de101ce952a56b37112b0fd183b802bc34ab1
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-09-27 15:48:57 -0400

    Update active_experiments_summary_rollup.sql

--- active_experiments_summary_rollup.sql
+++ active_experiments_summary_rollup.sql
@@ -1,139 +1,3 @@
------------------------------------------------------------------------------
---TAKE BASE FROM EXISTING ROLLUP, NEED TO ADD IN METRICS USING RESULTS_METRICS_DAY
------------------------------------------------------------------------------
-WITH all_experiments as (
-  SELECT
-  DISTINCT l.launch_id,
-  date(boundary_start_ts) AS start_date,
-  _date AS last_run_date,
-    case  
-    when bucketing_id_type = 1 then 'Browser'
-    else 'User'
-    end as bucketing_type 
-FROM
-  `etsy-data-warehouse-prod.catapult_unified.experiment` AS e
-INNER JOIN
-  `etsy-data-warehouse-prod.etsy_atlas.catapult_experiment_boundaries` AS b
-    ON UNIX_SECONDS(e.boundary_start_ts) = b.start_epoch
-    AND e.experiment_id = b.config_flag
-INNER JOIN
-  `etsy-data-warehouse-prod.etsy_atlas.catapult_launches` AS l ON l.launch_id = b.launch_id
-WHERE _date = CURRENT_DATE()-1
-  AND l.state = 1 -- Currently running
-  AND delete_date IS NULL -- Remove deleted experiments
-)
-, off_gms AS (
-  SELECT
-      exp_off_key_metrics.experiment_id,
-      exp_off_key_metrics.off_gms,
-      exp_off_key_metrics.off_prolist_spend * exp_off_key_metrics.off_browsers / 100 AS off_prolist_spend
-    FROM
-      `etsy-data-warehouse-prod`.catapult.exp_off_key_metrics
-    WHERE exp_off_key_metrics.segmentation = 'any'
-     AND exp_off_key_metrics.run_date IN(
-      SELECT
-          max(exp_on_key_metrics_1.run_date)
-        FROM
-          `etsy-data-warehouse-prod`.catapult.exp_on_key_metrics AS exp_on_key_metrics_1
-    )
-  ORDER BY
-    1
-)
-, exp_summary AS (
-  SELECT
-      -- a.experiment_id,
-      a.launch_id,
-      s.team,
-      s.name,
-      s.initiative,
-      a.bucketing_type,
-      -- a.treatments_per_experiment,
-      --s.enabling_teams,
-      s.launch_group as group_name,
-      s.outcome,
-      s.hypothesis,
-      s.launch_percentage,
-      a.start_date,
-      a.last_run_date,
-      date_diff(a.last_run_date, a.start_date, DAY) + 1 AS days_running,
-      o.off_gms,
-      o.off_prolist_spend,
-    FROM
-      all_experiments AS a
-      LEFT JOIN off_gms AS o ON a.launch_id = o.experiment_id
-      INNER JOIN `etsy-data-warehouse-prod`.etsy_atlas.catapult_launches AS s ON a.launch_id = s.launch_id
-    GROUP BY all
-)
-, daily_gms AS (
-  SELECT
-      transactions_gms_by_trans.date,
-      sum(transactions_gms_by_trans.trans_gms_gross) AS total_gms
-    FROM
-      `etsy-data-warehouse-prod`.transaction_mart.transactions_gms_by_trans
-    WHERE transactions_gms_by_trans.date >= (
-      SELECT
-          min(exp_summary.start_date)
-        FROM
-          exp_summary
-    )
-    GROUP BY 1
-  ORDER BY
-    1
-), daily_prolist_spend AS (
-  SELECT
-      prolist_daily_summary.date,
-      prolist_daily_summary.spend AS total_prolist_spend
-    FROM
-      `etsy-data-warehouse-prod`.rollups.prolist_daily_summary
-    WHERE prolist_daily_summary.date >= (
-      SELECT
-          min(exp_summary_0.start_date)
-        FROM
-          exp_summary AS exp_summary_0
-    )
-  ORDER BY
-    1
-), daily_denoms AS (
-  SELECT
-      g.date,
-      g.total_gms,
-      p.total_prolist_spend
-    FROM
-      daily_gms AS g
-      INNER JOIN daily_prolist_spend AS p ON g.date = p.date
-  ORDER BY
-    1
-), enabling_teams as (SELECT launch_id, STRING_AGG(DISTINCT `enabling_team_name` ORDER BY `enabling_team_name`) AS enabling_teams
-        FROM  `etsy-data-warehouse-prod.etsy_atlas.catapult_launches_enabling_team`
-        GROUP BY launch_id),
-SELECT
-    -- a_0.experiment_id,
-    a_0.launch_id,
-    a_0.bucketing_type,
-    -- a_0.treatments_per_experiment,
-    a_0.team,
-    a_0.name,
-    a_0.initiative,
-    a_0.group_name,
-    e.enabling_teams,
-    a_0.outcome,
-    a_0.hypothesis,
-    a_0.launch_percentage,
-    a_0.start_date,
-    a_0.last_run_date,
-    a_0.days_running,
-  FROM
-    exp_summary AS a_0
-    INNER JOIN daily_denoms AS d ON d.date BETWEEN a_0.start_date AND a_0.last_run_date
-    left join enabling_teams e on e.launch_id = a_0.launch_id
-   GROUP BY all
-ORDER BY
-  days_running
-
-
------------------------------------------------------------------------------
--- METRICS HERE
-----------------------------------------------------------------------------
  
  --grab all active experiments 
   WITH all_experiments as (
@@ -271,11 +135,12 @@ qualify metric_rnk=1 -- prrioitized cuped values for pval
     , max(case when lower(metric_display_name) = 'orders per converting browser (ocb)' and relative_change > 0 and is_significant = true then 1 else 0 end) as positive_significance_ocb
 
 --purchase frequency
-    , max(case when metric_display_name = 'Orders per Unit' then metric_value_control else null end) as control_opu
-    , max(case when metric_display_name = 'Orders per Unit' then metric_value_treatment else null end) as opu
-    , max(case when lower(metric_display_name) = 'total orders per unit'  then relative_change else null end)/100 as pct_change_opu
-    , max(case when lower(metric_display_name) = 'total orders per unit'  then p_value else null end) as pval_opu
-    , max(case when lower(metric_display_name) = 'total orders per unit'  then is_significant else null end) as significance_opu
+----should i use total 
+    , max(case when lower(metric_display_name) = 'total orders per unit' then metric_value_control else null end) as control_opu
+    , max(case when lower(metric_display_name) = 'total orders per unit' then metric_value_treatment else null end) as opu
+    , max(case when lower(metric_display_name) = 'total orders per unit'then relative_change else null end)/100 as pct_change_opu
+    , max(case when lower(metric_display_name) = 'total orders per unit' then p_value else null end) as pval_opu
+    , max(case when lower(metric_display_name) = 'total orders per unit' then is_significant else null end) as significance_opu
     , max(case when lower(metric_display_name) = 'total orders per unit' and relative_change > 0 and is_significant = true then 1 else 0 end) as positive_significance_opu
 
     , max(case when lower(metric_display_name) = 'percent with add to cart' then metric_value_control else null end) as control_atc
@@ -316,7 +181,6 @@ qualify metric_rnk=1 -- prrioitized cuped values for pval
   from metrics_list
 where 
   1=1
-  -- and launch_id = 1303095458918
   and metric_variant_name != 'off' --removed control as a metric_variant_name, but control metrics will still be there 
   group by all 
 )
@@ -336,7 +200,7 @@ where
   ORDER BY
     1
 )
--- , exp_summary AS (
+, exp_summary AS (
   SELECT
       -- a.experiment_id,
       a.launch_id,
@@ -691,12 +555,7 @@ where
       LEFT JOIN off_gms AS o ON a.launch_id = o.experiment_id
       INNER JOIN `etsy-data-warehouse-prod`.etsy_atlas.catapult_launches AS s ON a.launch_id = s.launch_id
     GROUP BY all
--- )
------------------------------------------------------------------------------
---EXTRAS TO ADD IN IF I CAN 
------------------------------------------------------------------------------
---includes active
-with platforms as (
+), plats as (
 -- This CTE gets the platform for each experiment (launch_id)
 select 
   distinct launch_id
@@ -704,7 +563,8 @@ select
   , name as platform
   , dense_rank() over(partition by launch_id  order by update_date desc) AS row_num
 FROM `etsy-data-warehouse-prod.etsy_atlas.catapult_launches_expected_platforms`
-qualify row_num = 1
+qualify 
+  row_num = 1
 )
 , plats_agg as (
 select 
@@ -730,7 +590,7 @@ SELECT
   , sum(case when name = "Shop Home" then 1 else 0 end) as shop_home
   , sum(case when name = "Home" then 1 else 0 end) as home
   , sum(case when name = "Sold Out Listing Page" then 1 else 0 end) as sold_out_listing
-  , sum(case when name = "Search" then 1 else 0 end) as search 
+  , sum(case when name = "Search" then 1 else 0 end) as _search 
 from `etsy-data-warehouse-prod.etsy_atlas.catapult_launches_expected_pages`
 where
   1=1
@@ -749,28 +609,10 @@ select
 from `etsy-data-warehouse-prod.catapult.results_coverage_day` 
 qualify date_rank=1
 )
-, exp_coverage_agg as (
 select
-  launch_id
-  , start_date
-  , end_date
-  , max(case when coverage_name = 'GMS coverage' then coverage_value else null end) as gms_coverage
-  , max(case when coverage_name = 'Traffic coverage' then coverage_value else null end) as traffic_coverage
-  , max(case when coverage_name = 'Offsite Ads coverage' then coverage_value else null end) as osa_coverage
-  , max(case when coverage_name = 'Prolist coverage' then coverage_value else null end) as prolist_coverage
-from exp_coverage
-group by  
-  all
-)
-, exp_metrics as (
--- This CTE gathers all the metric ids and the corresponding names included in the experiment, along with whether or not a metric is the success metric.
-select
-  cem.launch_id
-  , cem.metric_id
-  , cm.name
-  , cem.is_success_criteria
-from `etsy-data-warehouse-prod.etsy_atlas.catapult_experiment_metrics`  cem
-left join `etsy-data-warehouse-prod.etsy_atlas.catapult_metrics` cm
-  on cem.metric_id = cm.metric_id
-group by all -- has duplicate rows
-)
+  es.*,
+  platform
+from exp_summary es
+left join plats_agg pa using (launch_id)
+-- left join experiment_pages_ran_on ep
+--   on pa.launch_id=ep.launch_id
