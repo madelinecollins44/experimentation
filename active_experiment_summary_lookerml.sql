6d3df0d0b5a1e75d278a20f1a9ccbeb049ed803f
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-01 13:51:46 -0400

    Update active_experiment_summary_lookerml.sql

--- active_experiment_summary_lookerml.sql
+++ active_experiment_summary_lookerml.sql
@@ -183,14 +183,14 @@ view: active_experiment_summary_rollup {
 
   ##############measure
   measure: conversion_rate {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Conversion Rate"
     sql: ${TABLE}.conversion_rate ;;
     value_format: "0.00%"
   }
   measure: control_conversion_rate {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Conversion Rate"
     sql: ${TABLE}.control_conversion_rate ;;
@@ -198,546 +198,546 @@ view: active_experiment_summary_rollup {
   }
 
   measure: pct_change_conversion_rate {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Conversion Rate % Change"
     sql: ${TABLE}.pct_change_conversion_rate ;;
     value_format: "0.00%"
   }
   measure: pval_conversion_rate {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Conversion Rate P-Value"
     sql: ${TABLE}.pval_conversion_rate ;;
     value_format: "0.00"
   }
   measure: mean_visits {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Visit Frequency"
     sql: ${TABLE}.mean_visits ;;
     value_format: "0.00"
   }
   measure: control_mean_visits {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Visit Frequency"
     sql: ${TABLE}.control_mean_visits ;;
     value_format: "0.00"
   }
   measure: pct_change_mean_visits {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Visit Frequency % Change"
     sql: ${TABLE}.pct_change_mean_visits ;;
     value_format: "0.00%"
   }
   measure: pval_mean_visits {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Visit Frequency P-Value"
     sql: ${TABLE}.pval_mean_visits ;;
     value_format: "0.00"
   }
   measure: gms_per_unit {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "GMS per Unit"
     sql: ${TABLE}.gms_per_unit ;;
     value_format: "0.00"
   }
   measure: control_gms_per_unit {
-    type: sum
+    type: average
     group_label: "Control"
     label: "GMS per Unit"
     sql: ${TABLE}.control_gms_per_unit ;;
     value_format: "0.00"
   }
   measure: pct_change_gms_per_unit {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "GMS per Unit % Change"
     sql: ${TABLE}.pct_change_gms_per_unit ;;
     value_format: "0.00%"
   }
   measure: pval_gms_per_unit {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "GMS per Unit P-Value"
     sql: ${TABLE}.pval_gms_per_unit ;;
     value_format: "0.00"
   }
   measure: error_pg_view {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Error Pageview"
     sql: ${TABLE}.error_pg_view ;;
     value_format: "0.00%"
   }
   measure: control_percent_error_pg_view {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control % w/ Error Pageview"
     sql: ${TABLE}.control_percent_error_pg_view ;;
     value_format: "0.00%"
   }
   measure: pct_change_error_pg_view {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Error Pageview % Change"
     sql: ${TABLE}.pct_change_error_pg_view ;;
     value_format: "0.00%"
   }
   measure: pval_error_pg_view {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Error Pageview P-Value"
     sql: ${TABLE}.pval_error_pg_view ;;
     value_format: "0.00"
   }
   measure: mean_engaged_visit {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Engaged Visits"
     sql: ${TABLE}.mean_engaged_visit ;;
     value_format: "0.00"
   }
   measure: control_mean_engaged_visit {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Engaged Visits"
     sql: ${TABLE}.control_mean_engaged_visit ;;
     value_format: "0.00"
   }
   measure: pct_change_mean_engaged_visit {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Engaged Visits % Change"
     sql: ${TABLE}.pct_change_mean_engaged_visit ;;
     value_format: "0.00%"
   }
   measure: pval_mean_engaged_visit {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Engaged Visits P-Value"
     sql: ${TABLE}.pval_mean_engaged_visit ;;
     value_format: "0.00"
   }
   measure: bounces {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Bounces"
     sql: ${TABLE}.bounces ;;
     value_format: "0.00"
   }
   measure: control_bounces{
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Bounces"
     sql: ${TABLE}.control_bounces;;
     value_format: "0.00"
   }
   measure: pct_change_bounces {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Bounces % Change"
     sql: ${TABLE}.pct_change_bounces ;;
     value_format: "0.00%"
   }
   measure: pval_bounces {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Bounces P-Value"
     sql: ${TABLE}.pval_bounces ;;
     value_format: "0.00"
   }
   measure: pages_per_unit {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Pages per Unit"
     sql: ${TABLE}.pages_per_unit ;;
     value_format: "0.00"
   }
   measure: control_pages_per_unit {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Pages per Unit"
     sql: ${TABLE}.control_pages_per_unit ;;
     value_format: "0.00"
   }
   measure: pct_change_pages_per_unit{
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Pages per Unit % Change"
     sql: ${TABLE}.pct_change_pages_per_unit;;
     value_format: "0.00%"
   }
   measure: pval_pages_per_unit{
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Pages per Unit P-Value"
     sql: ${TABLE}.pval_pages_per_unit;;
     value_format: "0.00"
   }
   measure: ads_cvr {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Ads CR"
     sql: ${TABLE}.ads_cvr ;;
     value_format: "0.00"
   }
   measure: control_ads_cvr {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Ads CVR"
     sql: ${TABLE}.control_ads_cvr ;;
     value_format: "0.00"
   }
   measure: pct_change_ads_cvr{
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Ads CR % Change"
     sql: ${TABLE}.pct_change_ads_cvr;;
     value_format: "0.00%"
   }
   measure: pval_ads_cvr{
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Ads CR P-Value"
     sql: ${TABLE}.pval_ads_cvr;;
     value_format: "0.00"
   }
   measure: ads_acxv {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Ads ACXV"
     sql: ${TABLE}.ads_acxv ;;
     value_format: "0.00"
   }
   measure: control_ads_acxv {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Ads ACXV"
     sql: ${TABLE}.control_ads_acxv ;;
     value_format: "0.00"
   }
   measure: pct_change_ads_acxv{
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Ads ACXV % Change"
     sql: ${TABLE}.pct_change_ads_acxv;;
     value_format: "0.00%"
   }
   measure: pval_ads_acxv{
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Ads ACXV P-Value"
     sql: ${TABLE}.pval_ads_acxv;;
     value_format: "0.00"
   }
   measure: winsorized_acxv {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Winsorized ACXV"
     sql: ${TABLE}.winsorized_acxv ;;
     value_format: "0.00"
   }
   measure: control_winsorized_acxv {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Ads ACXV"
     sql: ${TABLE}.control_winsorized_acxv ;;
     value_format: "0.00"
   }
   measure: pct_change_winsorized_acxv{
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Winsorized ACXV % Change"
     sql: ${TABLE}.pct_change_winsorized_acxv;;
     value_format: "0.00%"
   }
   measure: pval_winsorized_acxv{
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Winsorized ACXV P-Value"
     sql: ${TABLE}.pval_winsorized_acxv;;
     value_format: "0.00"
   }
   measure: ocb {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Orders per Converting Browser"
     sql: ${TABLE}.ocb ;;
     value_format: "0.00"
   }
   measure: control_ocb {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Order per Converting Browser"
     sql: ${TABLE}.control_ocb ;;
     value_format: "0.00"
   }
   measure: pct_change_ocb {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Orders per Converting Browser % Change"
     sql: ${TABLE}.pct_change_ocb;;
     value_format: "0.00%"
   }
   measure: pval_ocb {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Orders per Converting Browser P-Value"
     sql: ${TABLE}.pval_ocb;;
     value_format: "0.00"
   }
   measure: opu {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Orders per User"
     sql: ${TABLE}.opu ;;
     value_format: "0.00"
   }
   measure: control_opu {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Order per User"
     sql: ${TABLE}.control_opu ;;
     value_format: "0.00"
   }
   measure: pct_change_opu {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Orders per User % Change"
     sql: ${TABLE}.pct_change_opu;;
     value_format: "0.00%"
   }
   measure: pval_opu {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Orders per User P-Value"
     sql: ${TABLE}.pval_opu;;
     value_format: "0.00"
   }
   measure: atc {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Add to Cart"
     sql: ${TABLE}.atc ;;
     value_format: "0.00"
   }
   measure: control_atc {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Add to Cart"
     sql: ${TABLE}.control_atc ;;
     value_format: "0.00"
   }
   measure: pct_change_atc {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Add to Cart % Change"
     sql: ${TABLE}.pct_change_atc;;
     value_format: "0.00%"
   }
   measure: pval_atc {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Add to Cart P-Value"
     sql: ${TABLE}.pval_atc;;
     value_format: "0.00"
   }
   measure: checkout_start {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Checkout Start"
     sql: ${TABLE}.checkout_start ;;
     value_format: "0.00"
   }
   measure: control_checkout_cart {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Checkout Start"
     sql: ${TABLE}.control_checkout_cart ;;
     value_format: "0.00"
   }
   measure: pct_change_checkout_start {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Checkout Start % Change"
     sql: ${TABLE}.pct_change_checkout_start;;
     value_format: "0.00%"
   }
   measure: pval_checkout_start {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Checkout Start P-Value"
     sql: ${TABLE}.pval_checkout_start;;
     value_format: "0.00"
   }
   measure: aov {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "AOV"
     sql: ${TABLE}.aov ;;
     value_format: "0.00"
   }
   measure: control_aov {
-    type: sum
+    type: average
     group_label: "Control"
     label: "AOV"
     sql: ${TABLE}.control_aov ;;
     value_format: "0.00"
   }
   measure: pct_change_aov {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "AOV % Change"
     sql: ${TABLE}.pct_change_aov;;
     value_format: "0.00%"
   }
   measure: pval_aov {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "AOV P-Value"
     sql: ${TABLE}.pval_aov;;
     value_format: "0.00"
   }
   measure: mean_prolist_spend {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Mean Prolist Spend"
     sql: ${TABLE}.mean_prolist_spend ;;
     value_format: "0.00"
   }
   measure: control_mean_prolist_spend {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control Mean Prolist Spend"
     sql: ${TABLE}.control_mean_prolist_spend ;;
     value_format: "0.00"
   }
   measure: pct_change_mean_prolist_spend {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Mean Prolist Spend % Change"
     sql: ${TABLE}.pct_change_mean_prolist_spend;;
     value_format: "0.00%"
   }
   measure: pval_mean_prolist_spend {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Mean Prolist Spend P-Value"
     sql: ${TABLE}.pval_mean_prolist_spend;;
     value_format: "0.00"
   }
   measure: mean_osa_revenue {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "Mean OSA Spend"
     sql: ${TABLE}.mean_osa_revenue ;;
     value_format: "0.00"
   }
   measure: control_mean_osa_revenue {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Mean OSA Revenue"
     sql: ${TABLE}.control_mean_osa_revenue ;;
     value_format: "0.00"
   }
   measure: pct_change_mean_osa_revenue {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "Mean OSA Spend % Change"
     sql: ${TABLE}.pct_change_mean_osa_revenue;;
     value_format: "0.00%"
   }
   measure: pval_mean_osa_revenue {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "Mean OSA Spend P-Value"
     sql: ${TABLE}.pval_mean_osa_revenue;;
     value_format: "0.00"
   }
   measure: pct_homescreen_exit {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "% Homescreen Exit "
     sql: ${TABLE}.pct_homescreen_exit ;;
     value_format: "0.00%"
   }
   measure: pct_change_pct_homescreen_exit {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "% Homescreen Exit % Change"
     sql: ${TABLE}.pct_change_pct_homescreen_exit;;
     value_format: "0.00%"
   }
   measure: control_pct_homescreen_exit {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control % Homescreen Exits"
     sql: ${TABLE}.control_pct_homescreen_exit ;;
     value_format: "0.00%"
   }
   measure: pval_pct_homescreen_exit {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "% Homescreen Exit P-Value"
     sql: ${TABLE}.pval_pct_homescreen_exit;;
     value_format: "0.00"
   }
   measure: pct_homescreen_clickthrough {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "% Homescreen Clickthrough"
     sql: ${TABLE}.pct_homescreen_clickthrough ;;
     value_format: "0.00%"
   }
   measure: pct_change_pct_homescreen_clickthrough {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "% Homescreen Clickthrough % Change"
     sql: ${TABLE}.pct_change_pct_homescreen_clickthrough;;
     value_format: "0.00%"
   }
   measure: control_pct_homescreen_clickthrough {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control % Homescreen Clickthrough"
     sql: ${TABLE}.control_pct_homescreen_clickthrough ;;
     value_format: "0.00%"
   }
   measure: pval_pct_homescreen_clickthrough {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "% Homescreen Clickthrough P-Value"
     sql: ${TABLE}.pval_pct_homescreen_clickthrough;;
     value_format: "0.00"
   }
   measure: pct_w_engagement_with_collected_content {
-    type: sum
+    type: average
     group_label: "Treatment"
     label: "% w/ Engagement + Collected Content"
     sql: ${TABLE}.pct_w_engagement_with_collected_content ;;
     value_format: "0.00%"
   }
   measure: control_pct_w_engagement_with_collected_content {
-    type: sum
+    type: average
     group_label: "Control"
     label: "Control % w/ Engagement + Collected Content"
     sql: ${TABLE}.control_pct_w_engagement_with_collected_content ;;
     value_format: "0.00%"
   }
   measure: pct_change_pct_w_engagement_with_collected_content {
-    type: sum
+    type: average
     group_label: "% Change"
     label: "% w/ Engagement + Collected Content % Change"
     sql: ${TABLE}.pct_change_pct_w_engagement_with_collected_content;;
     value_format: "0.00%"
   }
   measure: pval_pct_w_engagement_with_collected_content {
-    type: sum
+    type: average
     group_label: "P-Values"
     label: "% w/ Engagement + Collected Content P-Value"
     sql: ${TABLE}.pval_pct_w_engagement_with_collected_content;;
