c3d8339fad10291dfdfe85fbc431eaaa6f39e4eb
madelinecollins44 <150381098+madelinecollins44@users.noreply.github.com>
2024-10-15 15:05:29 -0600

    Update tablet_experiments.sql

--- tablet_experiments.sql
+++ tablet_experiments.sql
@@ -194,3 +194,61 @@ inner join
   on a.experiment_id=b.launch_id
 where launch_id = 1305470782469
 group by all
+
+------------------------------------------------
+--testing
+------------------------------------------------
+WITH experiments AS (
+  SELECT experiment_id, MAX(_date) AS _date
+  FROM `etsy-data-warehouse-prod.catapult.catapult_metrics_results` 
+  WHERE _date >= "2024-01-01" 
+    AND platform in ('iOS','Android') -- only BOE experiments 
+  GROUP BY all
+)
+, tablet_metrics as (
+WITH experiments AS (
+  SELECT experiment_id, MAX(_date) AS _date
+  FROM `etsy-data-warehouse-prod.catapult.catapult_metrics_results` 
+  WHERE _date >= "2024-01-01" 
+    AND platform in ('iOS','Android') -- only BOE experiments 
+  GROUP BY 1
+)
+  
+select
+  _date, 
+  experiment_id,
+  lower(name) as metric_name,
+  metric_id,
+  platform,
+  IF(segment = "1", "tablet", "not_tablet") AS is_tablet,
+  population_1_name as control,
+  population_2_name as treatment,
+  p_value, 
+  power as _power, 
+  percent_change,
+from 
+  `etsy-data-warehouse-prod.catapult.catapult_metrics_results` 
+INNER JOIN experiments USING (_date, experiment_id)
+inner join etsy-data-warehouse-prod.etsy_atlas.catapult_metrics using (metric_id)
+where 
+  _date > '2024-01-01'
+  AND segmentation = "is_tablet"
+  and experiment_id = 1305470782469
+  AND metric_id IN (
+    9542279976, # Conversion rate (non-CUPED)
+    653265516272, # Winsorized AC*V (non-CUPED)
+    1304192866864, # Mean engaged visits
+    47362065341, # Mean visits (non-CUPED)
+    1303188452741, # Orders per unit
+    1303200836305 # Winsorized AOV
+  )
+)
+select 
+  _date, 
+  experiment_id,
+  metric_id,
+ is_tablet,
+ count(*)
+ from tablet_metrics
+ group by all order by 5 desc
+---this is all unique 
